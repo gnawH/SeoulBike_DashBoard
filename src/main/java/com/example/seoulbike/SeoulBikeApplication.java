afcd7783c0f9446797c254ab4c83bb0ffdd94c7e
gnawH <sun30126331@gmail.com>
2026-04-08 17:22:23 +0900

    feat: create gitignore

---
+++ src/main/java/com/example/seoulbike/SeoulBikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.seoulbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulBikeApplication.class, args);
+	}
+
+}
