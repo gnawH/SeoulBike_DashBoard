775db3ac5db980dacb0ba4b5a5997f59d812cda2
Polar-Beary-Poby <primekk3502@gmail.com>
2026-04-09 20:12:32 +0900

    Auth 기능 Model

--- src/main/java/com/example/seoulbike/auth/dao/IMemberRepository.java
+++ src/main/java/com/example/seoulbike/auth/dao/IMemberRepository.java
@@ -3,13 +3,12 @@ package com.example.seoulbike.auth.dao;
 import java.util.List;
 
 import com.example.seoulbike.auth.model.Member;
-
 public interface IMemberRepository {
-    void insertMember(Member member); // 회원가입 (관리자만?)
+    void insertMember(Member member); // 회원가입
     Member selectMember(String userid); // 로그인
-    List<Member> selectAllMembers(); // 전체 회원 조회 (관리자만?)
-    void updateMember(Member member); // 회원 정보 수정 (유저 가능)
-    void deleteMember(Member member); // 회원 탈퇴 (관리자만)
+    List<Member> selectAllMembers(); // 전체 회원 조회
+    void updateMember(Member member); // 회원 정보 수정
+    void deleteMember(Member member); // 회원 탈퇴
     String getPassword(String userid); // 비밀번호 조회
 
 }
