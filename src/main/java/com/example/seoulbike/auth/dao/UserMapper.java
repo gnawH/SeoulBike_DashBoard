d0a478cf6bd10a54e9eaea1bdcec1be893432509
Polar-Beary-Poby <primekk3502@gmail.com>
2026-04-10 12:16:12 +0900

    fix: UserMapper

--- src/main/java/com/example/seoulbike/auth/dao/UserMapper.java
+++ src/main/java/com/example/seoulbike/auth/dao/UserMapper.java
@@ -2,12 +2,12 @@ package com.example.seoulbike.auth.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.seoulbike.auth.model.SignupDto;
+import com.example.seoulbike.auth.model.Signup;
 import com.example.seoulbike.auth.model.User;
 
 @Mapper
 public interface UserMapper {
 	User findByUserId(String userId);
-	void insertMember(SignupDto dto);
+	void insertMember(Signup dto);
 	}
 
