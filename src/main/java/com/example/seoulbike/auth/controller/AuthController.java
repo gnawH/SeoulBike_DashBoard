6a7a456464d6e105abe3234d4abfc18340fe88f8
unknown <jhd6290@gmail.com>
2026-04-10 12:42:09 +0900

    update: HomeController, AuthController 구조 변경

--- src/main/java/com/example/seoulbike/auth/controller/AuthController.java
+++ src/main/java/com/example/seoulbike/auth/controller/AuthController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/auth")
 public class AuthController {
 	
 	// 민호님 서비스 확인 후 연결
-	//@Autowired
+	//@Autowired                                                                   
 	//IAuthService  AuthService;
 	
 	//로그인 페이지
@@ -22,7 +22,7 @@ public class AuthController {
 	
 	//로그인 처리
 	@PostMapping("/login")
-	public String login(String userid, String password, Model model) {
+	public String login(String userid, String password, HttpSession session, Model model) {
 		try {
 			// JWT Service 연결
 			
@@ -43,7 +43,15 @@ public class AuthController {
 	@PostMapping("/signup")
 	public String signup(String userid, String name, String password,
 							String email, String region, Model model) {
-		return "redirect:/auth/login";
+		
+		try {
+			//Todo: Service 연결 후 구현하기
+			//authService.signup(signupDto);
+			return "redirect:/login";
+		}catch(Exception e){
+			model.addAttribute("message", "SIGNUP_FAIL");
+			return "auth/signup"; 
+		}
 	}
 	
     // 로그아웃
