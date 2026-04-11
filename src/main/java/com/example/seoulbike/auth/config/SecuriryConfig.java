8895a2c059577bee096085403d162b37a75d6299
unknown <jhd6290@gmail.com>
2026-04-11 23:14:00 +0900

    feat: HomeController 추가 및 전체 파일 반영

--- src/main/java/com/example/seoulbike/auth/config/SecuriryConfig.java
+++ src/main/java/com/example/seoulbike/auth/config/SecuriryConfig.java
@@ -7,9 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.example.seoulbike.auth.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
