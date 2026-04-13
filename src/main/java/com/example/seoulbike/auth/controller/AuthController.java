4410e1519aec4e46a56da17798667209177926e8
gnawH <sun30126331@gmail.com>
2026-04-13 20:12:28 +0900

    fix: 충돌 오류 재수정

--- src/main/java/com/example/seoulbike/auth/controller/AuthController.java
+++ src/main/java/com/example/seoulbike/auth/controller/AuthController.java
@@ -102,3 +102,112 @@ public class AuthController {
 	
 	
 }
+    //회원정보 수정 페이지
+    @GetMapping("/updateUser")
+    public String updateUserPage(HttpSession session, Model model, @RequestParam(name = "status", required = false) 
+    String status) {
+    	AuthResponse loginUser = (AuthResponse) session.getAttribute("loginUser");
+    	
+    	if (loginUser == null) {
+    		return "redirect:/login";
+    	}
+    	model.addAttribute("loginUser", loginUser);
+    	if (status != null) {
+    	    model.addAttribute("status", status);
+    	}
+    	return "auth/updateUser";
+    }
+    
+    //회원정보 수정 처리
+    @PostMapping("/auth/update")
+    public String updateUser(Signup signupDto, HttpSession session, Model model) {
+    	AuthResponse loginUser = (AuthResponse) session.getAttribute("loginUser");
+    	
+    	if (loginUser == null) {
+    		return "redirect:/login";
+    	}
+    	try {
+    		authService.updateUser(signupDto);
+    		return "redirect:/";
+    	}catch (RuntimeException e) {
+            model.addAttribute("message", e.getMessage());
+            return "auth/updateUser";
+        }catch (Exception e) {
+            model.addAttribute("message", "UPDATE_FAIL");
+            return "auth/updateUser";
+        }
+    }
+    
+    //회원정보 삭제 처리
+    @PostMapping("/deleteUser")
+    public String deleteUser(@RequestParam("password") String password, HttpSession session, 
+    		HttpServletResponse response, Model model) {
+    	AuthResponse loginUser = (AuthResponse) session.getAttribute("loginUser");
+    	
+    	if (loginUser == null) {
+    		return "redirect:/login";
+    	}
+    	try {
+    		// 비밀번호 검증 후 삭제
+            authService.deleteUser(loginUser.getUserId(), password);
+            
+            // JWT 쿠키 삭제 
+        	Cookie cookie = new Cookie("JWT", null);
+        	cookie.setHttpOnly(true);   
+        	cookie.setSecure(false);
+        	cookie.setPath("/");
+        	cookie.setMaxAge(0);
+        	response.addCookie(cookie);
+        	
+        	//세션 종료
+        	session.invalidate();
+        	return "redirect:/"; 
+            
+    	}catch (RuntimeException e) {
+            model.addAttribute("message", e.getMessage());
+            return "auth/updateUser";
+        } catch (Exception e) {
+            model.addAttribute("message", "DELETE_FAIL");
+            return "auth/updateUser";
+        }
+    }
+    
+    //비밀번호 검증 -> updateUser.html 자바 스크립트 부분
+    @PostMapping("/api/auth/verify-password")
+    @ResponseBody
+    public Map<String, Boolean> verifyPassword(@RequestParam("password") String password,
+            HttpSession session){
+    	AuthResponse loginUser = (AuthResponse) session.getAttribute("loginUser");
+    	
+    	if (loginUser == null) {
+    		return Map.of("success", false); // 키 값 (success, false)로 반환
+    	}
+    	try {
+    		authService.verifyPasswordWithSecurity(loginUser.getUserId(), password);
+    		return Map.of("success", true);
+    	}catch(Exception e) {
+    		return Map.of("success", false);
+    	}
+    }
+    
+    //비밀번호 재설정
+    @PostMapping("/auth/reset-password")
+    public String resetPassword(@RequestParam("newPassword") String newPassword, HttpSession session, Model model) {
+    	AuthResponse loginUser = (AuthResponse) session.getAttribute("loginUser");
+    	
+    	if (loginUser == null) {
+            return "redirect:/login";
+        }
+    	try {
+    		authService.updatePassword(loginUser.getUserId(), newPassword);
+    		return "redirect:/updateUser?status=RESET_SUCCESS";
+    	}catch (RuntimeException e) {
+            model.addAttribute("message", e.getMessage());
+            model.addAttribute("loginUser", loginUser);
+            return "auth/updateUser";
+    	}catch (Exception e) {
+            model.addAttribute("message", "RESET_FAIL");
+            model.addAttribute("loginUser", loginUser);
+            return "auth/updateUser";
+    	}
+    }
