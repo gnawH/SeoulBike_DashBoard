0e8bfb0e57b7de4621676483423d812ebfee5e5a
unknown <jhd6290@gmail.com>
2026-04-13 17:24:50 +0900

    feat: Add updatePasword method

--- src/main/java/com/example/seoulbike/auth/service/IAuthService.java
+++ src/main/java/com/example/seoulbike/auth/service/IAuthService.java
@@ -16,5 +16,7 @@ public interface IAuthService {
 
     // 회원정보 수정 시 비밀번호 재확인
     void verifyPasswordWithSecurity(String userId, String password);
-
+    
+    // 비밀번호 업데이트
+    void updatePassword(String userId, String password);
 }
